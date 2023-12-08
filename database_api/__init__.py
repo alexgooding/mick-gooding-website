0c36ea09a0e5fa60fe1279e526bfe44911782528
Alex Gooding <alexxgooding@gmail.com>
2023-12-08 18:56:47 +0000

    add product related endpoints

--- database_api/__init__.py
+++ database_api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .routes.painting_routes import painting_bp
+from .routes.product_routes import product_bp
 
 app = Flask(__name__)
 
@@ -7,5 +8,6 @@ def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(painting_bp, url_prefix="/api")
+    app.register_blueprint(product_bp, url_prefix="/api")
 
     return app
