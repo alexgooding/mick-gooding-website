bc8d2d16dc1bbb14c17f1e6fae0d3d10becf2d0b
Alex Gooding <alexxgooding@gmail.com>
2023-12-12 17:31:42 +0000

    create hello world react ui

--- database_api/__init__.py
+++ database_api/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes.painting_routes import painting_bp
 from .routes.product_routes import product_bp
 from .routes.cart_routes import cart_bp
 
-app = Flask(__name__)
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.register_blueprint(painting_bp, url_prefix="/api")
     app.register_blueprint(product_bp, url_prefix="/api")
