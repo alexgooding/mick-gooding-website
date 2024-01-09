bf44957db5e9a4b0ac0d13c15a05ad25797a0bd7
Alex Gooding <alexxgooding@gmail.com>
2024-01-09 16:27:48 +0000

    create summary column and style for multiple screen sizes

--- ui/src/components/Cart.js
+++ ui/src/components/Cart.js
@@ -10,9 +10,12 @@ const client = axios.create({
 });
 
 const Cart = () => {
+  const paypalLogoPath = `${process.env.PUBLIC_URL}/images/logos/transparent-paypal-logo.png`;
   const [products, setProducts] = useState([]);
   const [headerText, setHeaderText] = useState("");
-  const { getAllProductIds, getQuantityOfAllProducts } = useCart();
+  const { getAllProductIds, getQuantityOfProduct, getQuantityOfAllProducts } = useCart();
+  const deliveryFee = 0;
+  let cartTotal = 0;
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -38,16 +41,83 @@ const Cart = () => {
   }, [getAllProductIds]);
 
   return (
-    <div className="container">
+    <div className="container mb-4">
       <div className="row my-4">
         <span>
           {headerText}
         </span>
       </div>
-      <div className={products.length === 0 ? "invisible" : "row row-cols-1 shadow rounded-4"}>
-      {products.map((product) => (
-        <CartItem product={product} key={product.product_id}/>
-      ))}
+      <div className={products.length === 0 ? "invisible" : "row"}>
+        <div className="col-12 col-sm-12 col-md-8 flex-nowrap p-3 shadow rounded-4">
+          {products.map((product) => {
+            cartTotal += getQuantityOfProduct(product.product_id) * product.price;
+
+            return <CartItem product={product} key={product.product_id}/>
+          })}
+        </div>
+
+        <div className="col col-12 col-sm-12 col-md-4 p-3">
+          <div className="row flex-nowrap mb-2">
+            <div className="d-flex">
+              <b className="text-nowrap">Order Summary</b>
+            </div>
+          </div>
+          <div className="row flex-nowrap">
+            <div className="col">
+              <div className="d-flex">
+                <span className="text-nowrap">Item(s) total</span>
+              </div>
+            </div>
+            <div className="col">
+              <div className="d-flex justify-content-end">
+                <span className="text-nowrap">
+                  £{cartTotal.toFixed(2)}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="row flex-nowrap">
+            <div className="col">
+              <div className="d-flex">
+                <span className="text-nowrap">Delivery</span>
+              </div>
+            </div>
+            <div className="col">
+              <div className="d-flex justify-content-end">
+                <span className="text-nowrap">
+                  {deliveryFee === 0 ? "FREE" : `£${deliveryFee.toFixed(2)}`}
+                </span>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="row flex-nowrap mb-2">
+            <div className="col">
+              <div className="d-flex">
+                <b className="text-nowrap">
+                  Total ({getQuantityOfAllProducts()} items)
+                </b>
+              </div>
+            </div>
+            <div className="col">
+              <div className="d-flex justify-content-end">
+                <b>
+                  £{(cartTotal + deliveryFee).toFixed(2)}
+                </b>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <button className="payment-button btn btn-dark w-100 p-2 rounded-5" type="submit">
+                <span>
+                  Pay with&nbsp;  
+                  <img src={paypalLogoPath} width="50" />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>  
     </div>
   );
--- ui/src/components/CartItem.js
+++ ui/src/components/CartItem.js
@@ -29,50 +29,44 @@ const CartItem = ({ product }) => {
 
   return (
     <div className="row flex-nowrap">
-      <div className="col p-4">
-        <div className="d-flex">
-          <div className={`painting-image-container ${fullScreen ? "full-screen" : ""}`} onClick={toggleFullScreen}>
-            <img
-              src={fullScreen ? highResImagePath : lowResImagePath}
-              alt={`Painting: ${product.name}`}
-              className={`${fullScreen ? "full-screen-image" : "cart-item-image"}`}
-              onError={(e) => { e.target.src = defaultImagePath; }}
-            />
-          </div>
-          <div className="flex-column ms-4">
-            <div className="row">
-              <div className="d-flex description-item">
-                <b>{product.name}</b>
+      <div className="col col-9 p-4">
+        <div className="card border-0">
+          <div className="row g-0">
+            <div className="col col-12 col-sm-7 col-md-6 col-lg-5 col-xl-4 d-flex align-items-center justify-content-center">
+              <div className={`painting-image-container ${fullScreen ? "full-screen" : "img-fluid rounded-start ratio ratio-1x1"}`} onClick={toggleFullScreen}>
+                <img
+                  src={fullScreen ? highResImagePath : lowResImagePath}
+                  alt={`Painting: ${product.name}`}
+                  className={`${fullScreen ? "full-screen-image" : "cart-item-image"}`}
+                  onError={(e) => { e.target.src = defaultImagePath; }}
+                />
               </div>
             </div>
-            <div className="row">
-              <div className="d-flex description-item">
-                <span>{product.product_type}</span>
+            <div className="col col-12 col-sm-5 col-md-6 col-lg-7 col-xl-8">
+              <div className="card-body py-sm-0">
+                <b className="card-text description-item d-flex flex-nowrap">{product.name}</b>
+                <span className="card-text description-item d-flex flex-nowrap">{product.product_type}</span>
+                <div className="card-text description-item d-flex">
+                  <label htmlFor="quantitySelect" className="me-2">Quantity:</label>
+                  <select id="quantitySelect" defaultValue={quantity} onChange={(e) => handleQuantityChange(parseInt(e.target.value))}>
+                    {[...Array(11).keys()].map((value) => (
+                      <option key={value} value={value}>
+                        {value}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="card-text description-item d-flex">
+                  <button className="icon-button" title="Delete" onClick={(e) => handleQuantityChange(0)}>
+                    <CiTrash className="trash-icon"/>
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="d-flex description-item">
-                <label htmlFor="quantitySelect" className="me-2">Quantity:</label>
-                <select id="quantitySelect" defaultValue={quantity} onChange={(e) => handleQuantityChange(parseInt(e.target.value))}>
-                  {[...Array(11).keys()].map((value) => (
-                    <option key={value} value={value}>
-                      {value}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
-            <div className="row">
-              <div className="d-inline-flex">
-                <button className="icon-button" title="Delete" onClick={(e) => handleQuantityChange(0)}>
-                  <CiTrash className="trash-icon"/>
-                </button>
-              </div>
-            </div>
+            </div> 
           </div>
         </div>
       </div>
-      <div className="col p-4">
+      <div className="col col-3 p-4">
         <div className="d-flex justify-content-end">
           <span>£{(product.price * quantity).toFixed(2)}</span>
         </div>
