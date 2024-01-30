5f9a76852b7ffed76dce1fe2cb131def420c9d52
Alex Gooding <alexxgooding@gmail.com>
2024-01-30 17:29:19 +0000

    create unit tests for product routes

---
+++ tests/api/routes/test_product_routes.py
@@ -0,0 +1,169 @@
+import pytest
+from flask import Flask
+from functools import wraps
+from unittest.mock import patch, MagicMock
+from psycopg2 import OperationalError
+
+from api.routes import api as base_api
+from api.routes.product_routes import product_ns
+
+
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    app.testing = True
+    app.config['SERVER_NAME'] = 'localhost'
+
+    base_api.init_app(app)
+    base_api.add_namespace(product_ns, path='/api')
+
+    return app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def mock_db_operations(data, exception=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with patch('api.decorators.create_db_connection') as mock_db_connection:
+                mock_db_connection.return_value = MagicMock()
+
+                if exception:
+                    mock_db_connection.side_effect = exception
+
+                mock_cursor = mock_db_connection.return_value.cursor.return_value.__enter__.return_value
+                mock_cursor.fetchall = MagicMock()
+                mock_cursor.fetchone = MagicMock()
+
+                mock_cursor.fetchall.return_value = data
+                mock_cursor.fetchone.return_value = data
+
+                # Call the original test function
+                return func(*args, **kwargs)
+
+        return wrapper
+    return decorator
+
+
+@mock_db_operations(data={'data': 'fake_data'})
+def test_get_products_success(client):
+    response = client.get('/api/products')
+
+    assert response.status_code == 200
+    assert response.json == {'data': 'fake_data'}
+
+
+@mock_db_operations(data=None, exception=OperationalError('Invalid database credentials'))
+def test_get_products_invalid_credentials(client):
+    response = client.get('/api/products')
+
+    assert response.status_code == 401
+    assert response.json == {'message': "{OperationalError('Invalid database credentials')}"}
+
+
+@mock_db_operations(data={})
+def test_get_products_no_products_found(client):
+    response = client.get('/api/products')
+
+    assert response.status_code == 404
+    assert response.json == {'message': 'No products found. You have requested this URI [/api/products] but did you mean /api/products or /api/product/<int:product_id>/update-stock/<int:stock> or /api/product/<int:product_id>/all-info ?'}
+
+
+@mock_db_operations(data=None, exception=Exception("Test error"))
+def test_get_products_internal_server_error(client):
+    response = client.get('/api/products')
+
+    assert response.status_code == 500
+    assert response.json == {'message': 'Internal Server Error: Test error'}
+
+
+@mock_db_operations(data={'data': 'fake_data'})
+def test_get_product_success(client):
+    response = client.get('/api/product/1')
+
+    assert response.status_code == 200
+    assert response.json == {'data': 'fake_data'}
+
+
+@mock_db_operations(data=None, exception=OperationalError('Invalid database credentials'))
+def test_get_product_invalid_credentials(client):
+    response = client.get('/api/product/1')
+
+    assert response.status_code == 401
+    assert response.json == {'message': "{OperationalError('Invalid database credentials')}"}
+
+
+@mock_db_operations(data={})
+def test_get_product_not_found(client):
+    response = client.get('/api/product/1')
+
+    assert response.status_code == 404
+    assert response.json == {'message': 'Product not found. You have requested this URI [/api/product/1] but did you mean /api/product/<int:product_id>/update-stock/<int:stock> or /api/products or /api/product/<int:product_id>/all-info ?'}
+
+
+@mock_db_operations(data=None, exception=Exception("Test error"))
+def test_get_product_internal_server_error(client):
+    response = client.get('/api/product/1')
+
+    assert response.status_code == 500
+    assert response.json == {'message': 'Internal Server Error: Test error'}
+
+
+@mock_db_operations(data={'data': 'fake_data'})
+def test_get_product_all_info_success(client):
+    response = client.get('/api/product/1/all-info')
+
+    assert response.status_code == 200
+    assert response.json == {'data': 'fake_data'}
+
+
+@mock_db_operations(data=None, exception=OperationalError('Invalid database credentials'))
+def test_get_product_all_info_invalid_credentials(client):
+    response = client.get('/api/product/1/all-info')
+
+    assert response.status_code == 401
+    assert response.json == {'message': "{OperationalError('Invalid database credentials')}"}
+
+
+@mock_db_operations(data=None)
+def test_get_product_all_info_not_found(client):
+    response = client.get('/api/product/1/all-info')
+
+    assert response.status_code == 404
+    assert response.json == {'message': 'Product info not found. You have requested this URI [/api/product/1/all-info] but did you mean /api/product/<int:product_id>/all-info or /api/product/<int:product_id>/update-stock/<int:stock> or /api/products ?'}
+
+
+@mock_db_operations(data=None, exception=Exception("Test error"))
+def test_get_product_all_info_internal_server_error(client):
+    response = client.get('/api/product/1/all-info')
+
+    assert response.status_code == 500
+    assert response.json == {'message': 'Internal Server Error: Test error'}
+
+
+@mock_db_operations(data=None)
+def test_update_product_stock_success(client):
+    response = client.put('/api/product/1/update-stock/50')
+
+    assert response.status_code == 200
+    assert response.json == {'message': 'Stock updated successfully for product ID 1'}
+
+
+@mock_db_operations(data=None, exception=OperationalError('Invalid database credentials'))
+def test_update_product_stock_invalid_credentials(client):
+    response = client.put('/api/product/1/update-stock/50')
+
+    assert response.status_code == 401
+    assert response.json == {'message': "{OperationalError('Invalid database credentials')}"}
+
+
+@mock_db_operations(data=None, exception=Exception("Test error"))
+def test_update_product_stock_internal_server_error(client):
+    response = client.put('/api/product/1/update-stock/50')
+
+    assert response.status_code == 500
+    assert response.json == {'message': 'Internal Server Error: Test error'}
