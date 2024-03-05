caea2a22b3ff7eaa31263688822dd71dec3de1df
Alex Gooding <alexxgooding@gmail.com>
2024-03-05 17:15:19 +0000

    make footer padding smaller

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   };
 
   return (
-    <footer className="w-100 text-center mt-auto p-3 bg-white">
+    <footer className="w-100 text-center mt-auto p-1 bg-white">
       <div className="container text-center">
         <span className="text-muted" style={{ "font-size": "min(2vw, 0.75rem)" }}>
           © {getYear()} Mick Gooding Art. All rights reserved.
